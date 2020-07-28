95967ce1c7de234c9e88051121a3025d5a9105b8
jakeval <jakeval@google.com>
2020-07-28 10:11:47 -0600

    Added main file
    updated LICENSE with date and corp
    
    imported dependencies from main DH repo
    
    moved main.go to parent directory
    
    transpiled whistle to whistler
    
    cleaned main file
    
    updated build script
    
    Added copyright notices, updated bash scripts to fit styleguides, removed go.sum, removed local paths, and improved error handling

---
+++ build.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# Copyright 2020 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http:#www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# This script generates the appropriate go.mod files and builds the executable.
+
+if [[ -z "$1" ]]; then
+    echo "Usage: ./build [path/to/healthcare-data-harmonization]";
+else
+    ./go_mod_edits.sh "$1"
+    go build
+fi
---
+++ main.go
@@ -0,0 +1,48 @@
+// Copyright 2019 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package main runs the lineage graph generation program
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+
+	mappb "github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_engine/proto"
+	fileutil "github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_engine/util/ioutil"
+	"github.com/GoogleCloudPlatform/healthcare-data-harmonization/mapping_language/transpiler"
+)
+
+var (
+	mappingFile = flag.String("mapping_file_spec", "", "Mapping file (DHML file).")
+)
+
+func main() {
+	flag.Parse()
+
+	var mpc *mappb.MappingConfig
+	var err error
+
+	if *mappingFile == "" {
+		log.Fatalf("The whistle mapping file is not provided or is an empty string.\n" +
+			"Please provide file with -mapping_file_spec=/path/to/your-file.wstl")
+	}
+
+	if mpc, err = transpiler.Transpile(string(fileutil.MustRead(*mappingFile, "mapping"))); err != nil {
+		log.Fatalf("Transpiling whistle failed. %v", err)
+	}
+
+	fmt.Println(mpc.RootMapping)
+}
